518d993bc5044a4005202660f404ab2827ba31a6
Anastasiya-Moreva <nastena291086@mail.ru>
2022-04-17 18:24:45 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ export const startGame = (username, gameRound, description, numberRound = 3) => 
     }
     console.log('Correct!');
   }
-  console.log(`Congratulations, ${username}`);
+  console.log(`Congratulations, ${username}!`);
 };
 
 export default startGame;
